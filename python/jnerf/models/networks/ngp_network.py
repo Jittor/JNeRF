cc32f3452cf7ece42885c1c7e5d00799bcc92cf4
Gword <471184555@qq.com>
2022-06-02 09:56:11 +0800

    fix fmlp sm version support

--- python/jnerf/models/networks/ngp_network.py
+++ python/jnerf/models/networks/ngp_network.py
@@ -47,7 +47,7 @@ class NGPNetworks(nn.Module):
         self.pos_encoder = build_from_cfg(self.cfg.encoder.pos_encoder, ENCODERS)
         self.dir_encoder = build_from_cfg(self.cfg.encoder.dir_encoder, ENCODERS)
 
-        if self.use_fully and jt.flags.cuda_archs[0] >= 70:
+        if self.use_fully and jt.flags.cuda_archs[0] >= 75:
             assert self.pos_encoder.out_dim%16==0
             assert self.dir_encoder.out_dim%16==0
             self.density_mlp = FMLP([self.pos_encoder.out_dim, density_n_neurons, 16])
