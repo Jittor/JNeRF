f1504d0222675031bacfbc3c4481d5e4a2c0a66c
sleeplessai <2018063+sleeplessai@users.noreply.github.com>
2022-06-05 00:47:24 +0800

    add setup.py

---
+++ python/setup.py
@@ -0,0 +1,20 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="jnerf",
+    version="0.1",
+    description="instance-ngp and nerf benchmark base on jittor",
+    author="jittor team, g2 lab, tsinghua university",
+    url="https://github.com/Jittor/JNeRF",
+    packages=find_packages(),
+    install_requires=[
+        "jittor>=1.3.4.13",
+        "numpy",
+        "tqdm",
+        "opencv-python",
+        "Pillow",
+        "imageio",
+        "pyyaml"
+    ]
+)
+
