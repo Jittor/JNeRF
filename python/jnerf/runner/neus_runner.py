6ced235ea550bbf388f92a3f13bda529279b9f07
yqs <KevinYan37@163.com>
2022-09-30 16:15:00 +0800

    fusion neus

---
+++ python/jnerf/runner/neus_runner.py
@@ -0,0 +1,357 @@
+import os
+import time
+import logging
+import argparse
+import numpy as np
+import cv2 as cv
+import trimesh
+
+from shutil import copyfile
+from icecream import ic
+from tqdm import tqdm
+from pyhocon import ConfigFactory
+from jnerf.dataset.neus_dataset import NeuSDataset
+from jnerf.models.networks.neus_network import RenderingNetwork, SDFNetwork, SingleVarianceNetwork, NeRF
+from jnerf.models.samplers.neus_render.renderer import NeuSRenderer
+
+import jittor
+import jittor.nn as nn
+
+class NeuSRunner:
+    def __init__(self, conf_path, mode='train', case='CASE_NAME', is_continue=False):
+
+        # Configuration
+        self.conf_path = conf_path
+        f = open(self.conf_path)
+        conf_text = f.read()
+        conf_text = conf_text.replace('CASE_NAME', case)
+        f.close()
+
+        self.conf = ConfigFactory.parse_string(conf_text)
+        self.conf['dataset.data_dir'] = self.conf['dataset.data_dir'].replace('CASE_NAME', case)
+        self.base_exp_dir = self.conf['general.base_exp_dir']
+        os.makedirs(self.base_exp_dir, exist_ok=True)
+        self.dataset = NeuSDataset(self.conf['dataset'])
+        self.iter_step = 0
+
+        # Training parameters
+        self.end_iter = self.conf.get_int('train.end_iter')
+        self.save_freq = self.conf.get_int('train.save_freq')
+        self.report_freq = self.conf.get_int('train.report_freq')
+        self.val_freq = self.conf.get_int('train.val_freq')
+        self.val_mesh_freq = self.conf.get_int('train.val_mesh_freq')
+        self.batch_size = self.conf.get_int('train.batch_size')
+        self.validate_resolution_level = self.conf.get_int('train.validate_resolution_level')
+        self.learning_rate = self.conf.get_float('train.learning_rate')
+        self.learning_rate_alpha = self.conf.get_float('train.learning_rate_alpha')
+        self.use_white_bkgd = self.conf.get_bool('train.use_white_bkgd')
+        self.warm_up_end = self.conf.get_float('train.warm_up_end', default=0.0)
+        self.anneal_end = self.conf.get_float('train.anneal_end', default=0.0)
+
+        # Weights
+        self.igr_weight = self.conf.get_float('train.igr_weight')
+        self.mask_weight = self.conf.get_float('train.mask_weight')
+        self.is_continue = is_continue
+        self.mode = mode
+        self.model_list = []
+        self.writer = None
+
+        # Networks
+        params_to_train = []
+        self.nerf_outside = NeRF(**self.conf['model.nerf'])
+        self.sdf_network = SDFNetwork(**self.conf['model.sdf_network'])
+        self.deviation_network = SingleVarianceNetwork(**self.conf['model.variance_network'])
+        self.color_network = RenderingNetwork(**self.conf['model.rendering_network'])
+        params_to_train += list(self.nerf_outside.parameters())
+        params_to_train += list(self.sdf_network.parameters())
+        params_to_train += list(self.deviation_network.parameters())
+        params_to_train += list(self.color_network.parameters())
+
+        self.optimizer = jittor.optim.Adam(params_to_train, lr=self.learning_rate)
+
+        self.renderer = NeuSRenderer(self.nerf_outside,
+                                     self.sdf_network,
+                                     self.deviation_network,
+                                     self.color_network,
+                                     **self.conf['model.neus_renderer'])
+
+        # Load checkpoint
+        latest_model_name = None
+        if is_continue:
+            model_list_raw = os.listdir(os.path.join(self.base_exp_dir, 'checkpoints'))
+            model_list = []
+            for model_name in model_list_raw:
+                if model_name[-3:] == 'pkl' and int(model_name[5:-4]) <= self.end_iter:
+                    model_list.append(model_name)
+            model_list.sort()
+            latest_model_name = model_list[-1]
+
+        if latest_model_name is not None:
+            logging.info('Find checkpoint: {}'.format(latest_model_name))
+            self.load_checkpoint(latest_model_name)
+
+        # # Backup codes and configs for debug
+        # if self.mode[:5] == 'train':
+        #     self.file_backup()
+
+    def train(self):
+        self.update_learning_rate()
+        res_step = self.end_iter - self.iter_step
+        image_perm = self.get_image_perm()
+
+        for iter_i in tqdm(range(res_step)):
+            data = self.dataset.gen_random_rays_at(image_perm[self.iter_step % len(image_perm)], self.batch_size)
+
+            rays_o, rays_d, true_rgb, mask = data[:, :3], data[:, 3: 6], data[:, 6: 9], data[:, 9: 10]
+            near, far = self.dataset.near_far_from_sphere(rays_o, rays_d)
+
+            background_rgb = None
+            if self.use_white_bkgd:
+                background_rgb = jittor.ones([1, 3])
+
+            if self.mask_weight > 0.0:
+                mask = (mask > 0.5).float()
+            else:
+                mask = jittor.ones_like(mask)
+
+            mask_sum = mask.sum() + 1e-5
+            render_out = self.renderer.render(rays_o, rays_d, near, far,
+                                              background_rgb=background_rgb,
+                                              cos_anneal_ratio=self.get_cos_anneal_ratio())
+
+            color_fine = render_out['color_fine']
+            s_val = render_out['s_val']
+            cdf_fine = render_out['cdf_fine']
+            gradient_error = render_out['gradient_error']
+            weight_max = render_out['weight_max']
+            weight_sum = render_out['weight_sum']
+
+            # Loss
+            color_error = (color_fine - true_rgb) * mask
+            color_fine_loss = color_error.abs().sum() / mask_sum
+            psnr = 20.0 * jittor.log2(1.0 / (((color_fine - true_rgb)**2 * mask).sum() / (mask_sum * 3.0)).sqrt()) / jittor.log2(10)
+
+            eikonal_loss = gradient_error
+
+            mask_loss = jittor.nn.binary_cross_entropy_with_logits(weight_sum.safe_clip(1e-3, 1.0 - 1e-3), mask)
+
+            loss = color_fine_loss +\
+                eikonal_loss * self.igr_weight +\
+                mask_loss * self.mask_weight
+
+
+            self.optimizer.zero_grad()
+            self.optimizer.backward(loss)
+            self.optimizer.step()
+
+            self.iter_step += 1
+
+            if self.iter_step % self.report_freq == 0 :
+                print(self.base_exp_dir)
+                print('iter:{:8>d} loss = {} lr={}'.format(self.iter_step, loss, self.optimizer.param_groups[0]['lr']))
+
+            if self.iter_step % self.save_freq == 0:
+                self.save_checkpoint()
+
+            if self.iter_step % self.val_freq == 0:
+                self.validate_image()
+
+            if self.iter_step % self.val_mesh_freq == 0:
+                self.validate_mesh()
+
+            self.update_learning_rate()
+
+            if self.iter_step % len(image_perm) == 0:
+                image_perm = self.get_image_perm()
+
+    def get_image_perm(self):
+        return jittor.randperm(self.dataset.n_images)
+
+    def get_cos_anneal_ratio(self):
+        if self.anneal_end == 0.0:
+            return 1.0
+        else:
+            return np.min([1.0, self.iter_step / self.anneal_end])
+
+    def update_learning_rate(self):
+        if self.iter_step < self.warm_up_end:
+            learning_factor = self.iter_step / self.warm_up_end
+        else:
+            alpha = self.learning_rate_alpha
+            progress = (self.iter_step - self.warm_up_end) / (self.end_iter - self.warm_up_end)
+            learning_factor = (np.cos(np.pi * progress) + 1.0) * 0.5 * (1 - alpha) + alpha
+        for g in self.optimizer.param_groups:
+            g['lr'] = self.learning_rate * learning_factor
+
+    def file_backup(self):
+        dir_lis = self.conf['general.recording']
+        os.makedirs(os.path.join(self.base_exp_dir, 'recording'), exist_ok=True)
+        for dir_name in dir_lis:
+            cur_dir = os.path.join(self.base_exp_dir, 'recording', dir_name)
+            os.makedirs(cur_dir, exist_ok=True)
+            files = os.listdir(dir_name)
+            for f_name in files:
+                if f_name[-3:] == '.py':
+                    copyfile(os.path.join(dir_name, f_name), os.path.join(cur_dir, f_name))
+
+        copyfile(self.conf_path, os.path.join(self.base_exp_dir, 'recording', 'config.conf'))
+
+    def load_checkpoint(self, checkpoint_name):
+        checkpoint = jittor.load(os.path.join(self.base_exp_dir, 'checkpoints', checkpoint_name))
+        self.nerf_outside.load_state_dict(checkpoint['nerf'])
+        self.sdf_network.load_state_dict(checkpoint['sdf_network_fine'])
+        self.deviation_network.load_state_dict(checkpoint['variance_network_fine'])
+        self.color_network.load_state_dict(checkpoint['color_network_fine'])
+        self.optimizer.load_state_dict(checkpoint['optimizer'])
+        self.iter_step = checkpoint['iter_step']
+
+        logging.info('End')
+
+    def save_checkpoint(self):
+        checkpoint = {
+            'nerf': self.nerf_outside.state_dict(),
+            'sdf_network_fine': self.sdf_network.state_dict(),
+            'variance_network_fine': self.deviation_network.state_dict(),
+            'color_network_fine': self.color_network.state_dict(),
+            'optimizer': self.optimizer.state_dict(),
+            'iter_step': self.iter_step,
+        }
+
+        os.makedirs(os.path.join(self.base_exp_dir, 'checkpoints'), exist_ok=True)
+        jittor.save(checkpoint, os.path.join(self.base_exp_dir, 'checkpoints', 'ckpt_{:0>6d}.pkl'.format(self.iter_step)))
+
+    def validate_image(self, idx=-1, resolution_level=-1):
+        if idx < 0:
+            idx = np.random.randint(self.dataset.n_images)
+
+        print('Validate: iter: {}, camera: {}'.format(self.iter_step, idx))
+
+        if resolution_level < 0:
+            resolution_level = self.validate_resolution_level
+        rays_o, rays_d = self.dataset.gen_rays_at(idx, resolution_level=resolution_level)
+        H, W, _ = rays_o.shape
+        rays_o = rays_o.reshape(-1, 3).split(self.batch_size)
+        rays_d = rays_d.reshape(-1, 3).split(self.batch_size)
+
+        out_rgb_fine = []
+        out_normal_fine = []
+
+        for rays_o_batch, rays_d_batch in zip(rays_o, rays_d):
+            near, far = self.dataset.near_far_from_sphere(rays_o_batch, rays_d_batch)
+            background_rgb = jittor.ones([1, 3]) if self.use_white_bkgd else None
+
+            render_out = self.renderer.render(rays_o_batch,
+                                              rays_d_batch,
+                                              near,
+                                              far,
+                                              cos_anneal_ratio=self.get_cos_anneal_ratio(),
+                                              background_rgb=background_rgb)
+
+            def feasible(key): return (key in render_out) and (render_out[key] is not None)
+
+            if feasible('color_fine'):
+                out_rgb_fine.append(render_out['color_fine'].detach().numpy())
+            if feasible('gradients') and feasible('weights'):
+                n_samples = self.renderer.n_samples + self.renderer.n_importance
+                normals = render_out['gradients'] * render_out['weights'][:, :n_samples, None]
+                if feasible('inside_sphere'):
+                    normals = normals * render_out['inside_sphere'][..., None]
+                normals = normals.sum(dim=1).detach().numpy()
+                out_normal_fine.append(normals)
+            del render_out
+
+        img_fine = None
+        if len(out_rgb_fine) > 0:
+            img_fine = (np.concatenate(out_rgb_fine, axis=0).reshape([H, W, 3, -1]) * 256).clip(0, 255)
+
+        normal_img = None
+        if len(out_normal_fine) > 0:
+            normal_img = np.concatenate(out_normal_fine, axis=0)
+            rot = np.linalg.inv(self.dataset.pose_all[idx, :3, :3].detach().numpy())
+            normal_img = (np.matmul(rot[None, :, :], normal_img[:, :, None])
+                          .reshape([H, W, 3, -1]) * 128 + 128).clip(0, 255)
+
+        os.makedirs(os.path.join(self.base_exp_dir, 'validations_fine'), exist_ok=True)
+        os.makedirs(os.path.join(self.base_exp_dir, 'normals'), exist_ok=True)
+
+        for i in range(img_fine.shape[-1]):
+            if len(out_rgb_fine) > 0:
+                cv.imwrite(os.path.join(self.base_exp_dir,
+                                        'validations_fine',
+                                        '{:0>8d}_{}_{}.png'.format(self.iter_step, i, idx)),
+                           np.concatenate([img_fine[..., i],
+                                           self.dataset.image_at(idx, resolution_level=resolution_level)]))
+            if len(out_normal_fine) > 0:
+                cv.imwrite(os.path.join(self.base_exp_dir,
+                                        'normals',
+                                        '{:0>8d}_{}_{}.png'.format(self.iter_step, i, idx)),
+                           normal_img[..., i])
+
+    def render_novel_image(self, idx_0, idx_1, ratio, resolution_level):
+        """
+        Interpolate view between two cameras.
+        """
+        rays_o, rays_d = self.dataset.gen_rays_between(idx_0, idx_1, ratio, resolution_level=resolution_level)
+        H, W, _ = rays_o.shape
+        rays_o = rays_o.reshape(-1, 3).split(self.batch_size)
+        rays_d = rays_d.reshape(-1, 3).split(self.batch_size)
+
+        out_rgb_fine = []
+        for rays_o_batch, rays_d_batch in zip(rays_o, rays_d):
+            near, far = self.dataset.near_far_from_sphere(rays_o_batch, rays_d_batch)
+            background_rgb = jittor.ones([1, 3]) if self.use_white_bkgd else None
+
+            render_out = self.renderer.render(rays_o_batch,
+                                              rays_d_batch,
+                                              near,
+                                              far,
+                                              cos_anneal_ratio=self.get_cos_anneal_ratio(),
+                                              background_rgb=background_rgb)
+
+            out_rgb_fine.append(render_out['color_fine'].detach().cpu().numpy())
+
+            del render_out
+
+        img_fine = (np.concatenate(out_rgb_fine, axis=0).reshape([H, W, 3]) * 256).clip(0, 255).astype(np.uint8)
+        return img_fine
+
+    def validate_mesh(self, world_space=False, resolution=64, threshold=0.0):
+        bound_min = jittor.float32(self.dataset.object_bbox_min)
+        bound_max = jittor.float32(self.dataset.object_bbox_max)
+
+        vertices, triangles =\
+            self.renderer.extract_geometry(bound_min, bound_max, resolution=resolution, threshold=threshold)
+        os.makedirs(os.path.join(self.base_exp_dir, 'meshes'), exist_ok=True)
+
+        if world_space:
+            vertices = vertices * self.dataset.scale_mats_np[0][0, 0] + self.dataset.scale_mats_np[0][:3, 3][None]
+
+        mesh = trimesh.Trimesh(vertices, triangles)
+        mesh.export(os.path.join(self.base_exp_dir, 'meshes', '{:0>8d}.ply'.format(self.iter_step)))
+
+        logging.info('End')
+
+    def interpolate_view(self, img_idx_0, img_idx_1):
+        images = []
+        n_frames = 60
+        for i in range(n_frames):
+            print(i)
+            images.append(self.render_novel_image(img_idx_0,
+                                                  img_idx_1,
+                                                  np.sin(((i / n_frames) - 0.5) * np.pi) * 0.5 + 0.5,
+                          resolution_level=4))
+        for i in range(n_frames):
+            images.append(images[n_frames - i - 1])
+
+        fourcc = cv.VideoWriter_fourcc(*'mp4v')
+        video_dir = os.path.join(self.base_exp_dir, 'render')
+        os.makedirs(video_dir, exist_ok=True)
+        h, w, _ = images[0].shape
+        writer = cv.VideoWriter(os.path.join(video_dir,
+                                             '{:0>8d}_{}_{}.mp4'.format(self.iter_step, img_idx_0, img_idx_1)),
+                                fourcc, 30, (w, h))
+
+        for image in images:
+            writer.write(image)
+
+        writer.release()
---
+++ tools/run_neus.py
@@ -0,0 +1,41 @@
+from ast import parse
+import jittor as jt
+# from train import Trainer
+# from model import NerfNetworks, HuberLoss
+from tqdm import tqdm
+# from utils.dataset import  NerfDataset
+import argparse
+import numpy as np
+import os
+from jnerf.runner.neus_runner import NeuSRunner 
+from jnerf.utils.config import init_cfg
+# jt.flags.gopt_disable=1
+jt.flags.use_cuda = 1
+
+
+def main():
+
+    assert jt.flags.cuda_archs[0] >= 61, "Failed: Sm arch version is too low! Sm arch version must not be lower than sm_61!"
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--conf', type=str, default='./confs/base.conf')
+    parser.add_argument('--mode', type=str, default='train')
+    parser.add_argument('--mcube_threshold', type=float, default=0.0)
+    parser.add_argument('--is_continue', default=False, action="store_true")
+    parser.add_argument('--case', type=str, default='')
+    args = parser.parse_args()
+
+    runner = NeuSRunner(args.conf, args.mode, args.case, args.is_continue)
+
+    if args.mode == 'train':
+        runner.train()
+    elif args.mode == 'validate_mesh':
+        runner.validate_mesh(world_space=True, resolution=512, threshold=args.mcube_threshold)
+    elif args.mode.startswith('interpolate'):  # Interpolate views given two image indices
+        _, img_idx_0, img_idx_1 = args.mode.split('_')
+        img_idx_0 = int(img_idx_0)
+        img_idx_1 = int(img_idx_1)
+        runner.interpolate_view(img_idx_0, img_idx_1)
+    
+if __name__ == "__main__":
+    main()
