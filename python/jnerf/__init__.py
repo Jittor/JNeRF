3827b5c3e5813cea043658748e55b720989bb35d
Dun Liang <randonlang@gmail.com>
2022-10-28 12:15:36 +0800

    add special char check in directory

--- python/jnerf/__init__.py
+++ python/jnerf/__init__.py
@@ -1,13 +1,19 @@
+
+__version__ = '0.1.2.0'
+from jittor import LOG
+import os
+dirname = os.path.dirname(__file__)
+LOG.i(f"JNeRF({__version__}) at {dirname}")
+import sys
+assert sys.platform == "linux", "Windows/MacOS is not supported yet, everyone is welcome to contribute to this"
+
+sp_char = ' "\''
+for char in sp_char:
+    assert char not in dirname, f"Special char '{char}' detect in '{dirname}', please change jnerf to another directory and reinstall it."
+
 from . import dataset
 from . import models
 from . import ops
 from . import optims
 from . import runner
 from . import utils
-
-# version must use ' instead of "
-__version__ = '0.1.1.0'
-from jittor_utils import LOG
-LOG.i("JNeRF version ", __version__)
-import sys
-assert sys.platform == "linux", "Windows/MacOS is not supported yet, everyone is welcome to contribute to this"
